f1b5dfc849d6309e9d91190a152cd7a67fa09f42
lizuzhu <271551137@qq.com>
2016-05-25 15:57:44 +0800

    添加了Person self.view.backgroundColor = [UIColor cyanColor];

---
+++ gitHubTest/gitHubTest/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  gitHubTest
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ gitHubTest/gitHubTest/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  gitHubTest
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
